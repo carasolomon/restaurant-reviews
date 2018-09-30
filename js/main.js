1c71c358ced1407495bbb63e58793e70d42648bb
Cara Solomon <solomon.tacara@outlook.com>
2018-09-30 00:44:35 -0500

    added manifest.json

--- js/main.js
+++ js/main.js
@@ -190,7 +190,7 @@ const addMarkersToMap = (restaurants = self.restaurants) => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/js/sw.js').then((registration) => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     },(err) => {
       console.log('ServiceWorker registration failed: ', err);
